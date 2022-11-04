fe90b30dd2ad7832112170442960286565433528
phongsakorn <phongsakorn552@gmail.com>
2022-11-04 23:42:08 +0700

    edit lib

--- server/src/lib/cal.js
+++ server/src/lib/cal.js
@@ -7,9 +7,7 @@ let foods = x
 
 for (let index = 0; index < foods.length; index++) {
     let food = foods[index]
-    if(food.seafood ==false || food.seafood == true){
         allergy.push(food.name)
-    }
 }   
 for (let index = 0; index < 3; index++) {
     var item = allergy[Math.floor(Math.random()*allergy.length)];
