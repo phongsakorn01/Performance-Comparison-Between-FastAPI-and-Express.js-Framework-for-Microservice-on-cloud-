4b7ac55202e972a4310223ac9b6f9b02bad1f57d
phongsakorn <phongsakorn552@gmail.com>
2022-11-15 15:46:40 +0700

    delete

--- Django_server/Django_server/is_django/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class IsdjangoConfig(AppConfig):
-    name = 'is_django'
--- Django_server/Django_server/is_django/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from is_django import views 
- 
-urlpatterns = [ 
-    re_path(r'^users/users/$', views.is_django_list),
-   
-]
